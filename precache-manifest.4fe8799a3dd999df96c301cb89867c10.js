44b7dc9db1efe08be55601bb2a8393d8a457cc1d
Sterling Walsh <sterling.walsh.dev@gmail.com>
2019-02-12 21:07:58 -0500

    Updates

---
+++ precache-manifest.4fe8799a3dd999df96c301cb89867c10.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "3a6b9e2720604ebf7fed358e62442f9f",
+    "url": "/portfolio-react/static/media/robofriends.3a6b9e27.png"
+  },
+  {
+    "revision": "04557c1b0b790482dafb1bf4e082b69e",
+    "url": "/portfolio-react/static/media/react-hexagon.04557c1b.png"
+  },
+  {
+    "revision": "07c442a080c7582265b974fd1b4d97c2",
+    "url": "/portfolio-react/static/media/javascript-shield-logo.07c442a0.png"
+  },
+  {
+    "revision": "970b93f0f37f1af926ba32a3200c0879",
+    "url": "/portfolio-react/static/media/CSS3-Mark-Shape-Cut.970b93f0.png"
+  },
+  {
+    "revision": "9338e8a022e42c6bc02f",
+    "url": "/portfolio-react/static/js/runtime~main.9338e8a0.js"
+  },
+  {
+    "revision": "78eefe8bbdffe511736d",
+    "url": "/portfolio-react/static/js/main.78eefe8b.chunk.js"
+  },
+  {
+    "revision": "425e56b0a2282df30301",
+    "url": "/portfolio-react/static/js/1.425e56b0.chunk.js"
+  },
+  {
+    "revision": "78eefe8bbdffe511736d",
+    "url": "/portfolio-react/static/css/main.e1e2caba.chunk.css"
+  },
+  {
+    "revision": "baf5f1cc707a6869cf7a15eddb0e5e5b",
+    "url": "/portfolio-react/index.html"
+  }
+];
