11cc53d5879fb7142d50bad0db3cd03471b9ad4b
sterling.walsh.dev@gmail.com <sterling.walsh.dev@gmail.com>
2019-04-24 11:29:09 -0400

    Updates

---
+++ precache-manifest.655b700076c7a4368103335de03e6956.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "04557c1b0b790482dafb1bf4e082b69e",
+    "url": "/portfolio/static/media/react-hexagon.04557c1b.png"
+  },
+  {
+    "revision": "d72c2907c6ebb0953c60",
+    "url": "/portfolio/static/css/main.fb611d2e.chunk.css"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "e976d9803137d9c62ab9",
+    "url": "/portfolio/static/js/2.86bbef84.chunk.js"
+  },
+  {
+    "revision": "970b93f0f37f1af926ba32a3200c0879",
+    "url": "/portfolio/static/media/CSS3-Mark-Shape-Cut.970b93f0.png"
+  },
+  {
+    "revision": "07c442a080c7582265b974fd1b4d97c2",
+    "url": "/portfolio/static/media/javascript-shield-logo.07c442a0.png"
+  },
+  {
+    "revision": "d72c2907c6ebb0953c60",
+    "url": "/portfolio/static/js/main.de9341d2.chunk.js"
+  },
+  {
+    "revision": "9cab3941a9641e93c85fb1b45f2da6a2",
+    "url": "/portfolio/static/media/github-logo.9cab3941.png"
+  },
+  {
+    "revision": "3a6b9e2720604ebf7fed358e62442f9f",
+    "url": "/portfolio/static/media/robofriends.3a6b9e27.png"
+  },
+  {
+    "revision": "936017b590dac460c139264af6b12a5a",
+    "url": "/portfolio/static/media/facefind.936017b5.png"
+  },
+  {
+    "revision": "948efbd82b2d8b18e078de794890dcb5",
+    "url": "/portfolio/static/media/Matchbots.948efbd8.png"
+  },
+  {
+    "revision": "058d4c5ad75e9e90afc5ca4b537e2472",
+    "url": "/portfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio-react/precache-manifest.55f0bb19bc02f7126611cd750f440afc.js"
+  "/portfolio/precache-manifest.655b700076c7a4368103335de03e6956.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/portfolio-react/index.html", {
+workbox.routing.registerNavigationRoute("/portfolio/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
